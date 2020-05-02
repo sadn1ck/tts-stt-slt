69a20305aa22d24b388c8a974af80e99ffaf6ff8
Anik Das <anikdas0811@gmail.com>
2020-05-02 14:17:44 +0530

    Fixed button size

--- js/stt.js
+++ js/stt.js
@@ -2,41 +2,45 @@ window.addEventListener("DOMContentLoaded", () => {
     const button = document.getElementById("button");
     const result = document.getElementById("result");
     const main = document.getElementsByTagName("main")[0];
-    const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
-
-    let listening = false;
-    const recognition = new SpeechRecognition();
-    const start = () => {
-        recognition.start();
-        button.textContent = "Stop listening!";
-        main.classList.add("speaking");
-    };
-    const stop = () => {
-        recognition.stop();
-        button.textContent = "Start listening!";
-        main.classList.remove("speaking");
-    };
-    const onResult = event => {
-        // console.log(event);
-        result.innerHTML = "";
-        for (const res of event.results) {
-            const text = document.createTextNode(res[0].transcript);
-            const p = document.createElement("p");
-            // check if finished speaking
-            if (res.isFinal) {
-                p.classList.add("final");
-                // adds class to make text darker
+    const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition
+    if (typeof SpeechRecognition === "undefined") {
+        button.remove();
+        const message = document.getElementById("message");
+        message.removeAttribute("hidden");
+        message.setAttribute("aria-hidden", "false");
+    }
+    else {
+        let listening = false;
+        const recognition = new SpeechRecognition();
+        const start = () => {
+            recognition.start();
+            button.textContent = "Stop listening!";
+            main.classList.add("speaking");
+        };
+        const stop = () => {
+            recognition.stop();
+            button.textContent = "Start listening!";
+            main.classList.remove("speaking");
+        };
+        const onResult = event => {
+            // console.log(event);
+            result.innerHTML = "";
+            for (const res of event.results) {
+                const text = document.createTextNode(res[0].transcript);
+                const p = document.createElement("p");
+                if (res.isFinal) {
+                    p.classList.add("final");
+                }
+                p.appendChild(text);
+                result.appendChild(p);
             }
-            p.appendChild(text);
-            result.appendChild(p);
-        }
-    };
-    recognition.continuous = true;
-    recognition.interimResults = true;
-    recognition.addEventListener("result", onResult);
-    button.addEventListener("click", () => {
-        listening ? stop() : start();
-        listening = !listening;
-    });
-
+        };
+        recognition.continuous = true;
+        recognition.interimResults = true;
+        recognition.addEventListener("result", onResult);
+        button.addEventListener("click", () => {
+            listening ? stop() : start();
+            listening = !listening;
+        });
+    }
 });
